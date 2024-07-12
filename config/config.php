fd33a8f5106b612ca3b66421ed6ef1744163a327
BeibeiYang <chrisyangber@hotmail.com>
2024-07-13 05:58:09 +1000

    change config

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 
 $db = [
-  'dbms' => 'mysql',
-  'host' => 'localhost',
-  'port' => '3307',
-  'dbname' => 'themobilehour',
-  'charset' => 'utf8mb4',
-  'username' => 'tbh_admin',
-  'password' => '@.4Cn/MEp/Aix_)e'
+  'dbms' => '',
+  'host' => '',
+  'port' => '',
+  'dbname' => '',
+  'charset' => '',
+  'username' => '',
+  'password' => ''
 ];
 
 return $db;
